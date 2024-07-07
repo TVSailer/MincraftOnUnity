1c200d7be1b59df8dba79f2e471dd94d247a5c5d
Teregramister <teregeravalerij@gmail.com>
2024-07-07 19:01:06 +0300

    Added generation chunk

--- Assets/Scenes/ChunkRenderer.cs
+++ Assets/Scenes/ChunkRenderer.cs
@@ -1,23 +1,25 @@
 using System;
 using System.Collections.Generic;
+using Scenes;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class ChunkRenderer : MonoBehaviour
 {
-    private const int ChunkWidth = 10;
-    private const int ChunkHeight = 128;
+    public const int ChunkWidth = 10;
+    public const int ChunkHeight = 128;
     
-    public int[,,] Blocks = new int[ChunkWidth, ChunkHeight, ChunkWidth]; 
-    
+    public int[,,] Blocks = new int[ChunkWidth, ChunkHeight, ChunkWidth];
+
     private List<Vector3> verticies = new List<Vector3>();
+
     private List<int> triangles = new List<int>();
+
     private void Start()
     {
         Mesh chunkMesh = new Mesh();
 
-        Blocks[0, 0, 0] = 1;
-        Blocks[1, 0, 0] = 1;
+        Blocks = TerrainGenerator.GenerateTerrain();
         
         for (int x = 0; x < ChunkWidth; x++)
         {
@@ -41,18 +43,31 @@ public class ChunkRenderer : MonoBehaviour
 
     private void GenerateBlock(int x, int y, int z)
     {
-        if (Blocks[x,y,z] == 0) return;
-
         var blockPosition = new Vector3Int(x, y, z);
         
-        GenerateRightSide(blockPosition);
-        GenerateLeftSide(blockPosition);
-        GenerateFrontSide(blockPosition);
-        GenerateBackSide(blockPosition);
-        GenerateTopSide(blockPosition); 
-        GenerateButtonSide(blockPosition);
+        if (GetBlockAtPosition(blockPosition) == 0) return;
+        
+        if (GetBlockAtPosition(blockPosition + Vector3Int.right) == 0) GenerateRightSide(blockPosition);
+        if (GetBlockAtPosition(blockPosition + Vector3Int.left) == 0) GenerateLeftSide(blockPosition);
+        if (GetBlockAtPosition(blockPosition + Vector3Int.forward) == 0) GenerateFrontSide(blockPosition);
+        if (GetBlockAtPosition(blockPosition + Vector3Int.back) == 0) GenerateBackSide(blockPosition);
+        if (GetBlockAtPosition(blockPosition + Vector3Int.up) == 0) GenerateTopSide(blockPosition); 
+        if (GetBlockAtPosition(blockPosition + Vector3Int.down) == 0) GenerateButtomSide(blockPosition);
     }
 
+    private int GetBlockAtPosition(Vector3Int blockPosition)
+    {
+        if (blockPosition.x >= 0 && blockPosition.x < ChunkWidth &&
+            blockPosition.y >= 0 && blockPosition.y < ChunkHeight &&
+            blockPosition.z >= 0 && blockPosition.z < ChunkWidth)
+        {
+            return Blocks[blockPosition.x, blockPosition.y, blockPosition.z];
+        }
+        else
+        {
+            return 0;
+        }
+    }
     private void GenerateRightSide(Vector3Int blockPosition)
     {
         verticies.Add(new Vector3(1, 0, 0) + blockPosition);
@@ -98,7 +113,7 @@ public class ChunkRenderer : MonoBehaviour
 
         AddLastVerticiesSquare();
     }
-    private void GenerateButtonSide(Vector3Int blockPosition)
+    private void GenerateButtomSide(Vector3Int blockPosition)
     {
         verticies.Add(new Vector3(0, 0, 0) + blockPosition);
         verticies.Add(new Vector3(1, 0, 0) + blockPosition);
---
+++ Assets/Scenes/TerrainGenerator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Scenes
+{
+    public static class TerrainGenerator
+    {
+        public static int[,,] GenerateTerrain()
+        {
+            var result = new int[ChunkRenderer.ChunkWidth, ChunkRenderer.ChunkHeight, ChunkRenderer.ChunkWidth];
+            
+            for (int x = 0; x < ChunkRenderer.ChunkWidth; x++)
+            {
+                for (int z = 0; z < ChunkRenderer.ChunkWidth; z++)
+                {
+                    float height = Mathf.PerlinNoise(x * .2f, z * .2f) * 5 + 10;
+
+                    for (int y = 0; y < height; y++) result[x, y, z] = 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
